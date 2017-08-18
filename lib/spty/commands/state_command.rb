e7a6a6034c24a20ef06444d94959912011fde832
J Shamsul <jibone@gmail.com>
2017-08-19 03:16:39 +0800

    Simplify state command

---
+++ lib/spty/commands/state_command.rb
@@ -0,0 +1,14 @@
+module Spty::Command
+  class StateCommand
+    # Display the current state of the player.
+    ASCRIPT_PLAYER_STATE = <<-EOL
+      tell application "Spotify" to return player state
+    EOL
+    def self.call(_, command = 'state')
+      if Spty::Command::PlayerCommand.running?
+        player_state = Spty::AppleScriptRunner.(ASCRIPT_PLAYER_STATE)
+        puts "=> player #{player_state}"
+      end
+    end
+  end
+end
