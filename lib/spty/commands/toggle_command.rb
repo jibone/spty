3bb0f709e07e3d2873780e0580354b115e6fda81
J Shamsul <jibone@gmail.com>
2017-08-21 00:48:41 +0800

    Remove debug message

--- lib/spty/commands/toggle_command.rb
+++ lib/spty/commands/toggle_command.rb
@@ -5,7 +5,6 @@ module Spty::Command
       tell application "Spotify" to playpause
     EOL
     def self.call(_, command = 'toggle')
-      puts '------------->jhk'
       if Spty::Command::PlayerCommand.running?
         Spty::AppleScriptRunner.(ASCRIPT_PLAYER_TOGGLE)
         Spty::Command::StateCommand.(nil, command)
