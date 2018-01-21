172e72b902c96b198140394ff82543958981b9ec
J Shamsul <jibone@gmail.com>
2018-01-22 02:50:54 +0800

    update read me and help command

--- lib/spty/commands/help_command.rb
+++ lib/spty/commands/help_command.rb
@@ -4,7 +4,9 @@ module Spty::Command
       "usage: spty <command> [<args>]\n"\
       "\n"\
       "Player specific command\n"\
-      "  state             Display teh current player state\n"\
+      "  state             Display the current player state\n"\
+      "  play              Play the current track\n"\
+      "  pause             Pause the current track\n"\
       "  toggle            Toggle player play or pause\n"\
       "  skip              Skip the current track and play the next track\n"\
       "  replay            Replay the current track\n"\
@@ -12,6 +14,8 @@ module Spty::Command
       "  volume up         Increase the volume level by 10\n"\
       "  volume down       Decrease the volume level by 11\n"\
       "  volume [number]   Set the volume level to number\n"\
+      "  mute              Set the volume level to 0\n"\
+      "  unmute            Set the volume level to 60\n"\
       "\n"\
       "Track specific command\n"\
       "  info              Show information for the current track\n"\
